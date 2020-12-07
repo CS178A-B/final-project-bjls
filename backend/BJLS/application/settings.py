c8aca2c9ca2e483a606a4789ea09be9ddc41f7ed
jth06 <jhuh014@ucr.edu>
2020-12-07 15:17:10 -0800

    Added REACT functionality to the Django backend

--- backend/BJLS/application/settings.py
+++ backend/BJLS/application/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dateutil',
     'crispy_forms',
+    'leads',
+    'rest_framework',
+    'frontend',
     # Django Apps
     'application.account'
 
@@ -78,12 +81,17 @@ WSGI_APPLICATION = 'application.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': os.environ.get('POSTGRES_HOST'),
+        # 'USER': os.environ.get('POSTGRES_USER'),
+        # 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        # 'HOST': os.environ.get('POSTGRES_HOST'),
+        # 'PORT': os.environ.get('POSTGRES_PORT'),
         'NAME': 'tbeqdkda',
         'USER': 'tbeqdkda',
         'PASSWORD': 'l0RZXytXE1QLfU4WXyFGmiOgnD-Stv-n',
         'HOST': 'ruby.db.elephantsql.com',
-        'PORT': 5432,
+        'PORT': '5432',
     }
 }
 
@@ -125,7 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "application/static"), ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "application/static"),
+    os.path.join(BASE_DIR, "frontend/static"),
+ ]
 # STATICFILES_DIR = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
