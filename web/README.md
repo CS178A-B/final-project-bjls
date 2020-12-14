d3857d5d913f338814f3d08a2c2f3d9f564bb93a
Shuang Zhou <szhou061@ucr.edu>
2020-12-13 20:54:13 -0800

    Readme update

--- web/README.md
+++ web/README.md
@@ -1,1 +1,22 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+## Installation
+
+In this directory, run:
+
+### `npm install`
+
+This will install the corresponding dependency for the frontend structure.
+
+## How To Run
+In this directory, you can run:
+
+### `npm run dev`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+## Dependencies
+Install Node Package Manager (npm). [Helpful Documentation](https://www.npmjs.com/get-npm)
+
--- web/pages/dashboard/index.js
+++ web/pages/dashboard/index.js
@@ -1,96 +1,86 @@
 import React from "react";
 import styles from "../../styles/pages/Dashboard.module.css";
 import NavBar from "../../components/NavBar";
+import ProfileDrawer from "../../components/ProfileDrawer";
 import {
-    Button,
-    Card,
-    CardActionArea,
-    CardActions,
-    CardContent,
-    CardMedia,
-    Container,
-    createMuiTheme,
-    Grid,
-    Paper,
-    ThemeProvider,
-    Typography,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  createMuiTheme,
+  Grid,
+  Paper,
+  ThemeProvider,
+  Typography,
 } from "@material-ui/core";
 
 import axios from "axios";
 
 const JobCard = () => {
-    const theme = createMuiTheme({
-        palette: {
-            primary: {
-                main: "#a08dcc",
-            },
-        },
-    });
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#a08dcc",
+      },
+    },
+  });
 
-    return (
-        <React.Fragment>
-            <ThemeProvider theme={theme}>
-                <Grid item xs={4}>
-                    <Card className={styles.jobCard}>
-                        <CardActionArea>
-                            <CardMedia
-                                component="img"
-                                alt="jobcard"
-                                height="140"
-                                image="../../assets/cs141+.png"
-                                title="JobCard"
-                            />
-                            <CardContent>
-                                <Typography
-                                    gutterBottom
-                                    variant="h5"
-                                    component="h2"
-                                >
-                                    Ex veniam consectetur
-                                </Typography>
-                                <Typography
-                                    variant="body2"
-                                    color="textSecondary"
-                                    component="p"
-                                >
-                                    Reprehenderit incididunt sit occaecat
-                                    proident Lorem reprehenderit in. Excepteur
-                                    irure fugiat amet est sit. Elit Lorem eu
-                                    fugiat incididunt ut eu sint. Nulla officia
-                                    consectetur dolor qui labore exercitation.
-                                    Laboris adipisicing nisi officia ut mollit
-                                    cupidatat minim enim nisi est minim elit ex.
-                                    Mollit ea sit elit eiusmod eiusmod eiusmod
-                                    eiusmod sint veniam voluptate aliqua magna
-                                    commodo officia.
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size="small" color="primary">
-                                Apply
-                            </Button>
-                            <Button size="small" color="primary">
-                                Detail
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-            </ThemeProvider>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <Grid item xs={4}>
+          <Card className={styles.jobCard}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="jobcard"
+                height="140"
+                image="../../assets/cs141+.png"
+                title="JobCard"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Ex veniam consectetur
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Reprehenderit incididunt sit occaecat proident Lorem
+                  reprehenderit in. Excepteur irure fugiat amet est sit. Elit
+                  Lorem eu fugiat incididunt ut eu sint. Nulla officia
+                  consectetur dolor qui labore exercitation. Laboris adipisicing
+                  nisi officia ut mollit cupidatat minim enim nisi est minim
+                  elit ex. Mollit ea sit elit eiusmod eiusmod eiusmod eiusmod
+                  sint veniam voluptate aliqua magna commodo officia.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Apply
+              </Button>
+              <Button size="small" color="primary">
+                Detail
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </ThemeProvider>
+    </React.Fragment>
+  );
 };
 
 export default function DashBoard() {
-    const checkGreeting = () => {
-        if (Date.now.getHours < 12) {
-            return "Morning";
-        } else if (Date.now.getHour < 18) {
-            return "Afternoon";
-        } else {
-            return "Evening";
-        }
-    };
+  const checkGreeting = () => {
+    if (Date.now.getHours < 12) {
+      return "Morning";
+    } else if (Date.now.getHour < 18) {
+      return "Afternoon";
+    } else {
+      return "Evening";
+    }
+  };
 
     const getUserData = () => {
         let input = {
@@ -119,31 +109,31 @@ export default function DashBoard() {
                     Good {checkGreeting()}!{getUserData()}
                 </Typography>
 
-                <Grid
-                    container
-                    justify="center"
-                    spacing={5}
-                    className={styles.cardContainer}
-                >
-                    <Grid item xs={12}>
-                        <Typography variant="h4" component="h2" gutterBottom>
-                            Available Jobs
-                        </Typography>
-                    </Grid>
-                    {[1, 2, 3, 4, 5].map(() => {
-                        return <JobCard />;
-                    })}
+        <Grid
+          container
+          justify="center"
+          spacing={5}
+          className={styles.cardContainer}
+        >
+          <Grid item xs={12}>
+            <Typography variant="h4" component="h2" gutterBottom>
+              Available Jobs
+            </Typography>
+          </Grid>
+          {[1, 2, 3, 4, 5].map(() => {
+            return <JobCard />;
+          })}
 
-                    <Grid item xs={12} style={{ paddingTop: "15rem" }}>
-                        <Typography variant="h4" component="h2" gutterBottom>
-                            Pending Jobs
-                        </Typography>
-                    </Grid>
-                    {[1, 2, 3, 4, 5].map(() => {
-                        return <JobCard />;
-                    })}
-                </Grid>
-            </Container>
-        </>
-    );
+          <Grid item xs={12} style={{ paddingTop: "15rem" }}>
+            <Typography variant="h4" component="h2" gutterBottom>
+              Pending Jobs
+            </Typography>
+          </Grid>
+          {[1, 2, 3, 4, 5].map(() => {
+            return <JobCard />;
+          })}
+        </Grid>
+      </Container>
+    </>
+  );
 }
