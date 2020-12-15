65d24f9ea3d26c6dfee09498cf4ffa9deb4d1a13
jth06 <jhuh014@ucr.edu>
2020-12-14 16:25:56 -0800

    Added Job API at http://localhost:8000/api/job/

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserList
+from .views import UserList, JobList
 
 app_name = 'api'
 
 urlpatterns = [
-    path('api/user/', UserList.as_view(), name='api_user')
+    path('api/user/', UserList.as_view(), name='api_user'),
+    path('api/job/', JobList.as_view(), name='api_job')
 ]
