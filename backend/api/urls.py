eb03244d9869d3418fc82100b7202d51c5961646
Shuang Zhou <szhou061@ucr.edu>
2020-12-14 16:52:39 -0800

    Modify the backend to support JWT auth

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import UserList
+from .views import current_user, UserList
+from rest_framework_jwt.views import obtain_jwt_token
 
 app_name = 'api'
 
 urlpatterns = [
-    path('api/user/', UserList.as_view(), name='api_user')
+    path('api/user/', UserList.as_view(), name='api_user'),
+    path('api/current_user', current_user),
+    path('api/token-auth/', obtain_jwt_token)
 ]
