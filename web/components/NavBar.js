21ae53488f24032724d0d66d3360018bca999c5e
Shuang Zhou <szhou061@ucr.edu>
2020-12-17 05:07:04 -0800

    Added ProfileDrawer and update NavBar for intergration

--- web/components/NavBar.js
+++ web/components/NavBar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import styles from "../styles/components/NavBar.module.css";
-import React from "react";
+import React, { createRef, useRef, useState } from "react";
 import { createMuiTheme, makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -10,40 +10,50 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { ThemeProvider } from "@material-ui/styles";
 
+import ProfileDrawer from "./ProfileDrawer";
+
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: "#c1cc98",
-        },
-        secondary: {
-            main: "#d2bdff",
-            dark: "#a08dcc",
-            light: "#fff0ff",
-        },
+  palette: {
+    primary: {
+      main: "#c1cc98",
     },
+    secondary: {
+      main: "#d2bdff",
+      dark: "#a08dcc",
+      light: "#fff0ff",
+    },
+  },
 });
 
-export default function NavBar() {
-    return (
-        <ThemeProvider theme={theme}>
-            <div className={styles.root}>
-                <AppBar position="static" color="primary">
-                    <Toolbar>
-                        <IconButton
-                            edge="start"
-                            className={styles.menuButton}
-                            color="inherit"
-                            aria-label="menu"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6" className={styles.title}>
-                            Dashboard
-                        </Typography>
-                        <Button color="inherit">Log out</Button>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        </ThemeProvider>
-    );
+export default function NavBar({ UserInfo, handleLogout }) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ProfileDrawer drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen} />
+      <div className={styles.root}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={styles.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={() => {
+                setDrawerOpen(true);
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={styles.title}>
+              Dashboard
+            </Typography>
+            <Button color="inherit" onClick={handleLogout}>
+              Log out
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </ThemeProvider>
+  );
 }
