7deaa73125643b1c4fc7c15e8c395b3e487f17b8
jth06 <jhuh014@ucr.edu>
2020-12-17 22:43:52 -0800

    Added to requirements.txt

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,7 +6,7 @@ app_name = 'api'
 
 urlpatterns = [
     path('api/user/', UserList.as_view(), name='api_user'),
-    path('api/current_user', current_user),
+    path('api/current_user/', current_user),
     path('api/token-auth/', obtain_jwt_token),
     path('api/job/', JobList.as_view(), name='api_job')
 ]
