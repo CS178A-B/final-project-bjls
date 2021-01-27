02d5c99fd4107c175349198d2f0c55c44e963f02
Shuang Zhou <szhou061@ucr.edu>
2021-01-27 03:12:48 -0800

    JobSIngleLinelist for dashboard

--- web/components/JobCard.js
+++ web/components/JobCard.js
@@ -12,43 +12,37 @@ import {
 const JobCard = ({ name, description, poster }) => {
     return (
         <React.Fragment>
-            <Grid item xs={4}>
-                <Card>
-                    <CardActionArea>
-                        <CardContent>
-                            <Typography
-                                gutterBottom
-                                variant="h5"
-                                component="h2"
-                            >
-                                {name}
-                            </Typography>
-                            <Typography
-                                variant="body2"
-                                color="textSecondary"
-                                component="p"
-                            >
-                                {description}
-                            </Typography>
-                            <Typography
-                                variant="body2"
-                                color="textSecondary"
-                                component="p"
-                            >
-                                {poster}
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                        <Button size="small" color="secondary">
-                            Apply
-                        </Button>
-                        <Button size="small" color="secondary">
-                            Detail
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
+            <Card>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {name}
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                        >
+                            {description}
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                        >
+                            {poster}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="secondary">
+                        Apply
+                    </Button>
+                    <Button size="small" color="secondary">
+                        Detail
+                    </Button>
+                </CardActions>
+            </Card>
         </React.Fragment>
     );
 };
---
+++ web/src/MockData.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        name: "CS178",
+        description:
+            "Looking for eager CS / CE / CSBA students looking to get involved in my Machine Learning Lab",
+        poster: "Liam Robbins",
+    },
+    {
+        name: "Undergruate Research",
+        description:
+            "Looking for eager CS / CE / CSBA students looking to get involved in my Machine Learning Lab",
+        poster: "John Huh",
+    },
+    {
+        name: "Undergruate Research",
+        description:
+            "Looking for eager CS / CE / CSBA students looking to get involved in my Machine Learning Lab",
+        poster: "Shuang Zhou",
+    },
+    {
+        name: "Undergruate Research",
+        description:
+            "Looking for eager CS / CE / CSBA students looking to get involved in my Machine Learning Lab",
+        poster: "Biqian Cheng",
+    },
+];
