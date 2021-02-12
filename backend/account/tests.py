8eb4fa2b8d2117c158cecd6e1702e2dd938890c0
liam-robbins <lrobb001@ucr.edu>
2021-02-11 16:45:31 -0800

    making travis happy

--- backend/account/tests.py
+++ backend/account/tests.py
@@ -2,10 +2,8 @@ from django.test import TestCase
 from .models import User, Student, Faculty, Job
 # Create your tests here.
 class BasicTest(TestCase):
+    def setUp(self):
+        Student.objects.create(name="automated_testing",major="Auto Test", GPA=4.01, completeness=101)
     def test_fields(self):
-        student1 = Student()
-        student1.name = "liam"
-        student1.major = "Computer Engineering"
-        student1.GPA = 3.93
-        student1.completeness = 25
-        
+        s = Student.objects.get(name="automated_testing")
+        self.assertEqual(s.major, "Auto Test")
