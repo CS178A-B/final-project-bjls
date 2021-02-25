9f36e1382c803f810b0bd62a94eff2fbc57fa38a
Shuang Zhou <szhou061@ucr.edu>
2021-02-24 22:17:20 -0800

    Adding some api call

--- web/utils/ApiCall.js
+++ web/utils/ApiCall.js
@@ -1,18 +1,63 @@
 import axios from "axios";
 
-const fetchUserData = () => {
-        axios
-            .get("http://localhost:8000/api/job", {
-                headers: {
-                    Authorization: `JWT ${localStorage.getItem("token")}`,
-                },
-            })
-            .then((r) => {
-                console.log(r);
-                setJobData(r.data);
-            })
-            .catch((e) => {
-                console.log(e.response);
-            });
-    }
+const fetchJobs = (token) => {
+    axios
+        .get("http://localhost:8000/api/job", {
+            headers: {
+                Authorization: `JWT ${token}`,
+            },
+        })
+        .then((r) => {
+            console.log(r);
+            return r.data;
+        })
+        .catch((e) => {
+            console.log(e.response);
+            return e.response;
+        });
+};
+
+const fetchUserData = (token) => {
+    axios
+        .get("http://localhost:8000/api/current-user", {
+            headers: {
+                Authorization: `JWT ${token}`,
+            },
+        })
+        .then((r) => {
+            console.log(r);
+            return r.data();
+        })
+        .catch((e) => {
+            console.log(e.response);
+        });
+};
+
+const logInApi = (loginInfo) => {
+    axios
+        .post("http://localhost:8000/api/token-auth/", loginInfo)
+        .then((r) => {
+            console.log(r);
+            if (r.status === 200) {
+                window.localStorage.setItem("token", r.data.token);
+            }
+            return r.data();
+        })
+        .catch((error) => {
+            console.log(error.response);
+            return error.response;
+        });
+};
+
+const signUpApi = (signUpInfo) => {
+    axios
+        .post("http://localhost:8000/api/user/", signUpInfo)
+        .then((r) => {
+            console.log(r);
+            return r.data;
+        })
+        .catch((error) => {
+            console.log(error.response);
+            return error.response.data;
+        });
 };
