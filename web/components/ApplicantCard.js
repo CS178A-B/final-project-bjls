adc7654a1c8b9a581707c0255a6decbb4a37d743
Shuang Zhou <szhou061@ucr.edu>
2021-02-26 03:40:32 -0800

    ApplicantCard added

---
+++ web/components/ApplicantCard.js
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+    Button,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    Divider,
+    Grid,
+    Typography,
+} from "@material-ui/core";
+
+const ApplicantCard = ({
+    name,
+    description,
+    poster,
+    setPostOpen,
+    handlePopover,
+    handlePopoverClose,
+}) => {
+    return (
+        <React.Fragment>
+            <Card>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {name}
+                        </Typography>
+                        <Divider />
+                        <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                        >
+                            Description:
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                        >
+                            {description}
+                        </Typography>
+
+                        <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                        >
+                            Poster: {poster}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button
+                        size="small"
+                        variant="contained"
+                        color="primary"
+                        disableElevation
+                        onClick={() => {
+                            setPostOpen(true);
+                        }}
+                    >
+                        Check Applicants
+                    </Button>
+                </CardActions>
+            </Card>
+        </React.Fragment>
+    );
+};
+
+export default ApplicantCard;
