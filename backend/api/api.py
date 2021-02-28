fe385b5052cf23807cb64310a106aaa2fcc5b1ec
jth06 <jhuh014@ucr.edu>
2021-02-28 15:18:35 -0800

    Added updated API to api.py

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,6 +1,6 @@
-from account.models import User, Job
+from account.models import User, Job, Student, Faculty, Course
 from rest_framework import viewsets, permissions
-from .serializers import UserSerializer, JobSerializer
+from .serializers import UserSerializer, JobSerializer, StudentSerializer, FacultySerializer, CourseSerializer
 
 # Lead Viewset
 class UserViewSet(viewsets.ModelViewSet):
@@ -17,3 +17,24 @@ class JobViewSet(viewsets.ModelViewSet):
     ]
     serializer_class = JobSerializer
     
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = StudentSerializer
+
+class FacultyViewSet(viewsets.ModelViewSet):
+    queryset = Faculty.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = FacultySerializer
+    
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = CourseSerializer
